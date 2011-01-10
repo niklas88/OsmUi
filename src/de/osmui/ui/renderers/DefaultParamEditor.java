6992eaa6ab6c10f39af7391b493f22de7c596c87
Niklas Schnelle <niklas@komani.de>
2011-01-10 16:14:01 +0100

    ParameterBox renderer/editor

---
+++ src/de/osmui/ui/renderers/DefaultParamEditor.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package de.osmui.ui.renderers;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.TableCellEditor;
+
+import de.osmui.i18n.I18N;
+
+import de.osmui.model.pipelinemodel.AbstractParameter;
+
+/**
+ * @author niklas
+ * 
+ */
+public class DefaultParamEditor  extends AbstractCellEditor
+			implements TableCellEditor, ActionListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7189134812752397954L;
+
+	private JTextField textField;
+	
+	private AbstractParameter currParam;
+	
+	public DefaultParamEditor() {
+		textField = new JTextField();
+		textField.addActionListener(this);
+	}
+
+	@Override
+	public Component getTableCellEditorComponent(JTable table, Object value,
+			boolean isSelected, int row, int column) {
+		currParam = (AbstractParameter) value;
+		textField.setText(currParam.getValue());
+		return textField;
+
+	}
+
+	@Override
+	public Object getCellEditorValue() {
+		return  currParam;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String formerValue = currParam.getValue();
+		try {
+			currParam.setValue(textField.getText());
+			fireEditingStopped();
+		} catch (IllegalArgumentException e){
+			JOptionPane.showMessageDialog(null,I18N.getString("ParamBox.ValWrong"));
+			currParam.setValue(formerValue);
+		}
+	}
+}
---
+++ src/de/osmui/ui/renderers/ParamValueRenderer.java
@@ -0,0 +1,27 @@
+package de.osmui.ui.renderers;
+
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+import de.osmui.model.pipelinemodel.AbstractParameter;
+
+
+
+public class ParamValueRenderer extends DefaultTableCellRenderer {
+
+	private static final long serialVersionUID = 4215729606603179888L;
+
+	public Component getTableCellRendererComponent(JTable table, Object obj,
+			boolean isSelected, boolean hasFocus, int row, int column) {
+		super.getTableCellRendererComponent(table, obj, isSelected, hasFocus, row, column);
+		
+		AbstractParameter param = (AbstractParameter) obj;
+		//this.setText((task.getFriendlyName()!= null)?task.getFriendlyName():task.getName());
+		this.setText(param.getValue());
+		this.setToolTipText(param.getDescription().getDescription());
+		
+		return this;
+	}
+}
